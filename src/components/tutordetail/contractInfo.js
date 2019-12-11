68b074f53088c83d827d1542139e820abc6b8121
Kha Leo <lexuankha2409@gmail.com>
2019-12-11 21:34:06 +0700

    add fix path

---
+++ src/components/tutordetail/contractInfo.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Table, Tag } from 'antd';
+import Moment from 'react-moment';
+
+const constractTable = props => {
+  const { contracts } = props;
+  console.log(props);
+  const columns = [
+    {
+      title: 'Tên người đặt',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Ngày đặt',
+      dataIndex: 'term',
+      key: 'term',
+      render: val => <Moment format="DD/MM/YYYY">{val}</Moment>,
+    },
+    {
+      title: 'Tổng chi phí',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Tình trạng',
+      key: 'status',
+      dataIndex: 'status',
+      render: tags => (
+        <span>
+          {tags.map(tag => {
+            let color = 'green';
+            if (tag === 'fail') {
+              color = 'volcano';
+            }
+            if (tag === 'success') {
+              color = 'green';
+            }
+            if (tag === 'pending') {
+              color = 'orange';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </span>
+      ),
+    },
+  ];
+
+  const data = [];
+  if (contracts !== false) {
+    contracts.forEach(v => {
+      data.push({
+        key: '1',
+        name: v.name,
+        term: v.endTime,
+        price: `${v.totalPrice} VND`,
+        status: [v.status],
+      });
+    });
+  }
+
+  return (
+    <div className="contractInfo">
+      <Table style={{ height: '100%' }} columns={columns} dataSource={data} scroll={{ y: 320 }} />
+    </div>
+  );
+};
+
+export default constractTable;
